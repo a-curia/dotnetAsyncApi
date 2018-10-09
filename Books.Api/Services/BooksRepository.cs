c658a667b0483f717e7c70fb05074122773cf684
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2018-10-09 10:39:50 +0300

    first sample

---
+++ Books.Api/Services/BooksRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Books.Api.Contexts;
+using Books.Api.Entities;
+
+namespace Books.Api.Services
+{
+    public class BooksRepository : IBooksRepository
+    {
+        private BooksContext _context;
+
+        // we want these methods to access DB using EF, so we need the book context injected
+        public BooksRepository(BooksContext booksContext)
+        {
+            _context = booksContext ?? throw new ArgumentNullException(nameof(booksContext)); // in case context is null throw exception
+        }
+        public Task<Book> GetBookAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Book>> GetBooksAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Books.Api/Services/IBooksRepository.cs
@@ -0,0 +1,19 @@
+﻿using Books.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Api.Services
+{
+    interface IBooksRepository
+    {
+
+        //IEnumerable<Book> GetBooks();
+        //Book GetBook(Guid id);
+
+        Task<IEnumerable<Book>> GetBooksAsync();
+        Task<Book> GetBookAsync(Guid id);
+
+    }
+}
